30320fa6bed2174fa31a31a02eed4ab663fd5376
Code-Gita <codegita@gmail.com>
2022-04-16 18:39:57 +0530

    Add python driver to insert into blockchain

--- smart_contract/Student_SchoolInfo.sol
+++ smart_contract/Student_SchoolInfo.sol
@@ -10,18 +10,34 @@ contract Student
         address student_addr;
         address school_addr;
         string student_name;
+        uint student_id;
     }
+
+    event log_details(string, student_info);
+
+    uint student_id;
 
     mapping(address => student_info) student_view;
 
     mapping(address => student_info[]) school_view;
 
+    constructor()
+    {
+        student_id++;
+    }
+
     function add_student(address st_adr, address sch_adr, string memory st_name) public returns (bool)
     {
+        console.log("student_view[st_adr].student_id : ", student_view[st_adr].student_id);
+        require(student_view[st_adr].student_id == 0, "STUDENT ALREADY EXISTS IN DB.");
         student_info memory temp_s_info;
         temp_s_info.student_addr = st_adr;
         temp_s_info.school_addr = sch_adr;
         temp_s_info.student_name = st_name;
+        student_id++;
+        temp_s_info.student_id = student_id;
+        emit log_details("student_info added : ", temp_s_info);
+
         student_view[st_adr] = (temp_s_info);
         school_view[sch_adr].push(temp_s_info);
         return true;
